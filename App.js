1eaacc4b59d36827ae3a0c8bcbba15306c878873
rafaGomezRem <rafael@rem.tools>
2023-07-09 13:52:32 -0600

    feat: add images for project and app.js file

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+    <View>
+      <Text>App</Text>
+    </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default App
